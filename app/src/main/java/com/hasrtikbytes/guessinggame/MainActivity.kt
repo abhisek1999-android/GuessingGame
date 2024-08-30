21475948cc3b56fb4f32f9cc83e38bdd79d29000
Abhisek <abhiseal45@gmail.com>
2024-08-30 22:55:48 +0530

    Button click listener added !

--- app/src/main/java/com/hasrtikbytes/guessinggame/MainActivity.kt
+++ app/src/main/java/com/hasrtikbytes/guessinggame/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.hasrtikbytes.guessinggame
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var guessButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        guessButton = findViewById(R.id.guess_button)
+
+        guessButton.setOnClickListener {
+            //Log.d("MESSAGE", "onCreate: Button Clicked!! ")
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
         }
+
     }
 }
